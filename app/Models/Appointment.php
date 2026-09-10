3fbef112894d9ca0cf14a89a26d01282c1b8c526
alddebaran <rgcarrasco.pe@gmail.com>
2026-09-10 01:48:55 -0500

    feat: add functions in models

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Appointment extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function vehicle(): BelongsTo
+    {
+        return $this->belongsTo(Vehicle::class);
+    }
 }
--- app/Models/Client.php
+++ app/Models/Client.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Client extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function vehicles(): HasMany
+    {
+        return $this->hasMany(Vehicle::class);
+    }
+
+    public function appointments(): HasMany
+    {
+        return $this->hasMany(Appointment::class);
+    }
+
+    public function getFullNameAttribute(): string
+    {
+        return "{$this->nombres} {$this->apellidos} ({$this->numero_documento})";
+    }
 }
